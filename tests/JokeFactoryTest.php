f1090beafb05d2a6829adc688f9fb660a0265574
Gavin Walshe <30451215+gavinwalshe@users.noreply.github.com>
2020-09-05 23:07:16 +0800

    Apply fixes from StyleCI (#1)

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -7,32 +7,32 @@ use PHPUnit\Framework\TestCase;
 
 class JokeFactoryTest extends TestCase
 {
-	/** @test */
-	public function it_returns_a_random_joke()
-	{
-		$jokes = new JokeFactory([
-			'This is a joke',
-		]);
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertSame('This is a joke', $joke);
-	}
+        $this->assertSame('This is a joke', $joke);
+    }
 
-/** @test */
-	public function it_returns_a_predefined_joke()
-	{
-		$chuckNorrisJokes = [
-			'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
-			'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
-			'Chuck Norris counted to infinity... Twice.',
-			'They once made a "Chuck Norris" brand toilet paper, but it wouldn\'t take shit from anybody.'
-		];
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris\' tears cure cancer. Too bad he has never cried.',
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris counted to infinity... Twice.',
+            'They once made a "Chuck Norris" brand toilet paper, but it wouldn\'t take shit from anybody.',
+        ];
 
-		$jokes = new JokeFactory();
+        $jokes = new JokeFactory();
 
-		$joke = $jokes->getRandomJoke();
+        $joke = $jokes->getRandomJoke();
 
-		$this->assertContains($joke, $chuckNorrisJokes);
-	}
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
 }
